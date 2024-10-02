d062ff6d9257ccd9e377b556373654903012ac6e
ka-karot <anshuayan1@gmail.com>
2024-10-02 21:59:33 +0530

    dd

--- plugins/start.py
+++ plugins/start.py
@@ -94,7 +94,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_photo(
-            photo=START_PIC
+            photo=START_PIC,
             caption = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
